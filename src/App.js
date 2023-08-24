bb012dc61c6e386ddc0f4de112188c792beb1bb3
Imbisat-cc <Imbisat.haque@rockms.co.uk>
2023-08-24 13:25:40 +0500

    Improvised dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import About from './component/About';
+// import About from './component/About';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
 
 function App() {
+  const [themeColor, setThemeColor] = useState('light');
+
+  const toggleThemeColor = () => {
+    if (themeColor === 'light') {
+      setThemeColor('dark');
+      document.body.style.backgroundColor = "#292c32";
+    } else {
+      setThemeColor('light');
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
       {/* <Navbar  /> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={themeColor} toggleThemeColor={toggleThemeColor} />
       <div className="container  my-3">
-        <TextForm heading="Enter the text to analyze" />
+        <TextForm heading="Enter the text to analyze" mode={themeColor}/>
 
         {/* <About/> */}
 
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -25,7 +25,7 @@ export default function TextForm(props) {
     }
 
     const handlePaste = () => {
-        setText(text);
+        setText(text.concat(text));
     }
 
     const handleExtraSpaces = () => {
@@ -37,26 +37,31 @@ export default function TextForm(props) {
         setText('');
     }
 
+    const textColor = props.mode === 'light' ? 'black' : 'white';
+    const backgroundColor = props.mode === 'light' ? 'white' : '#292c32';
+
     return (
         <>
-            <div>
-                <h1>{props.heading}</h1>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <div className="container" style={{ color: textColor }}>
+                <div className="container my-3" >
+                    <h1>{props.heading}</h1>
+                    <div className="mb-3">
+                        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{ background: backgroundColor, color: textColor }}></textarea>
+                    </div>
+                    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                    <button className="btn btn-primary" onClick={handleCopy}>Copy Text</button>
+                    <button className="btn btn-primary mx-2" onClick={handlePaste}>Paste Copied Text</button>
+                    <button className="btn btn-primary" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                    <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-2" onClick={handlePaste}>Paste Copied Text</button>
-                <button className="btn btn-primary" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
-            </div>
-            <div className="container my-3">
-                <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} Character</p>
-                <p>{0.008 * text.split(" ").length} Minutes to read</p>
-                <h2>Preview</h2>
-                <p>{text}</p>
+                <div className="container my-3">
+                    <h1>Your Text Summary</h1>
+                    <p>{text.split(" ").length} words and {text.length} Character</p>
+                    <p>{0.008 * text.split(" ").length} Minutes to read</p>
+                    <h2>Preview</h2>
+                    <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+                </div>
             </div>
         </>
     )
